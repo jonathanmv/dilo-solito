67415740ce6156d54006c1036e99dbaaf2706d72
jonathanmv <jonathanmv@outlook.com>
2022-11-14 16:57:10 +0100

    added cover image and description to community

--- packages/app/features/community/community-screen.tsx
+++ packages/app/features/community/community-screen.tsx
@@ -1,8 +1,9 @@
-import { H1, H2, TextLink } from 'app/design/typography'
+import { H1, H2, P, TextLink } from 'app/design/typography'
 import { FlatList, Row } from 'app/design/layout'
 import { View } from 'app/design/view'
 
 import { createParam } from 'solito'
+import { ImageBackground } from 'react-native'
 
 const { useParam } = createParam<{ id: string }>()
 
@@ -12,8 +13,30 @@ const CHAT_ROOMS: {id: string, title: string}[] = [
     { id: 'urbanapes.hh.stpauli.events', title: 'Events'},
 ]
 
+type Community = {
+    id: string,
+    name: string,
+    description: string,
+    coverImage: string,
+}
+const COMMUNITIES: Community[] = [
+    {
+        id: 'urbanapes',
+        name: 'Urban Apes',
+        description: "Bouldern und danach feiern gehen - Wer nach dem Bouldern noch Kraft und Lust hat, kann den Tag auch ein paar Etagen höher im Uebel & Gefährlich beim Tanzen ausklingen lassen oder mit einem Bier durch die nahe Schanze bummeln",
+        coverImage: 'https://www.urbanapes.de/wp-content/uploads/2022/03/urbanapes_hamburgstpauli_boulderer_2_800x533.jpg'
+    },
+    {
+        id: 'rehbar',
+        name: 'REH BAR',
+        description: 'Mo - So 10 Uhr bis Open End - Caipirinha Happy Hour bis 21 Uhr - #dukommstdochrehrum - Trinken hilft immer.',
+        coverImage: 'https://www.urbanapes.de/wp-content/uploads/2022/03/urbanapes_hamburgstpauli_boulderer_6_800x533.jpg'
+    }
+]
+
 export function CommunityScreen() {
   const [id] = useParam('id')
+  const community = COMMUNITIES.find(c => c.id === id)
 
   const renderChatRoomItem = ({ item }) => {
     return (
@@ -25,10 +48,15 @@ export function CommunityScreen() {
   return (
     <View className="flex-1 justify-between bg-green-300 items-stretch">
       <Row className="bg-red-300">
-        <H1>Community {id}</H1>
+        <ImageBackground source={{uri: community?.coverImage }}>
+            <H1 className='text-slate-100 drop-shadow flex-1'>{community?.name}</H1>
+        </ImageBackground>
       </Row>
+      <View className="max-w-xl">
+        <P>{community?.description}</P>
+      </View>
       <View className="flex-1 bg-blue-300">
-        <H2>Chat rooms {id}</H2>
+        <H2>Chat rooms</H2>
         <FlatList
             data={CHAT_ROOMS}
             renderItem={renderChatRoomItem}
