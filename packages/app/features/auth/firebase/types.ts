62f25a7f27cc9420c14d73e786ac5efa0a68a572
jonathanmv <jonathanmv@outlook.com>
2022-11-15 10:19:16 +0100

    add auth-related files

---
+++ packages/app/features/auth/firebase/types.ts
@@ -0,0 +1,14 @@
+// from https://github.com/nandorojo/nextjs-conf-22-example/blob/setup/packages/app/features/auth/firebase/types.ts
+import type * as firebase from 'firebase/auth'
+
+type User = Pick<firebase.User, 'uid'>
+
+export type Firebase = {
+  getIsSignedIn: () => boolean
+  signInAnonymously: () => Promise<User>
+  signOut: () => Promise<void>
+  onAuthStateChanged: (
+    callback: (user: { uid: string } | null) => void
+  ) => () => void
+  getCurrentUser: () => User | null
+}
